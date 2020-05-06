377756a6ee6d7748ce9e2a8fe2589f5ab073a7e0
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-05-06 18:13:01 +0500

    footer main added

--- src/components/Footer/FooterComp.js
+++ src/components/Footer/FooterComp.js
@@ -11,47 +11,77 @@ import "./FooterComp.css";
 const FooterComp = () => {
   return (
     <div>
-      <div className="Red">
-        <h1>S</h1>
+      <div className="footerMain">
+        <div className="footerCol">
+          <h1>Payment Methods</h1>
+          <img
+            src="https://www.nextgen.pk/wp-content/uploads/2019/03/payments-methods-e1557944732347.png"
+            alt="Payment Methods"
+          />
+        </div>
+        <div className="footerCol">
+          <h1>Policies</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            consectetur tempus condimentum. Praesent ut dictum felis. Maecenas
+            placerat urna urna, vitae consequat lacus ultricies et.
+          </p>
+        </div>
+        <div className="footerCol">
+          <h1>Other Projects</h1>
+          <ul>
+            <li>
+              <a href="#">Robofriends</a>
+            </li>
+            <li>
+              <a href="#">Face rocgination</a>
+            </li>
+            <li>
+              <a href="#">Covid 19 (new)</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      Developed by: Sajeel Aalam
-      <br />
-      <div className="footerSocialIcons">
-        <a
-          href="https://github.com/imsajeel"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GithubOutlined />
-        </a>
-        <a
-          href="https://www.facebook.com/imsajeelaalam"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FacebookOutlined />
-        </a>
-        <a
-          href="https://www.instagram.com/imsajeelaalam"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <InstagramOutlined />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/sajeel-aalam-4075861a2/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LinkedinOutlined />
-        </a>
-        <a
-          href="https://dribbble.com/imsajeel"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <DribbbleOutlined />
-        </a>
+      <div className="footerBottom">
+        Developed by: Sajeel Aalam
+        <br />
+        <div className="footerSocialIcons">
+          <a
+            href="https://github.com/imsajeel"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubOutlined />
+          </a>
+          <a
+            href="https://www.facebook.com/imsajeelaalam"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FacebookOutlined />
+          </a>
+          <a
+            href="https://www.instagram.com/imsajeelaalam"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramOutlined />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sajeel-aalam-4075861a2/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinOutlined />
+          </a>
+          <a
+            href="https://dribbble.com/imsajeel"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <DribbbleOutlined />
+          </a>
+        </div>
       </div>
     </div>
   );
